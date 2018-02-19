63449745edb5345748db6faba1d8fa7dc7413211
Александр <misch.sania2017@yandex.ru>
2018-02-19 12:25:05 +0500

    c

---  MakePictrue.py
+++  MakePictrue.py
@@ -9,9 +9,13 @@ def download(url, name):#downloading the map
     out.write(img)
     out.close()
     return Image.open(name)
+def condition(col):
+    return col[0]+col[2]-col[1]<50 and col[1]>col[2] and col[1]>col[0]
 def diff(col,R,G,B):#difference between colors
     return math.sqrt((col[0]-R)**2+(col[1]-G)**2+(col[2]-B)**2)
- 
+def getColors(url, name):
+    picture=download(url, name)
+    return picture.load()
 Coordinates=np.array([56.854886, 60.573581,56.812489, 60.648804])#starting and ending coordinates
  
 step=0.000488
@@ -22,24 +26,21 @@ for Lat in np.arange(Coordinates[1],  Coordinates[3], step):#получение 
     for Long in np.arange(Coordinates[0], Coordinates[2],-step):#проход по долготе с шагом
         y+=1
         LatLong=str(Long)+","+str(Lat)
-        sputnik=download('https:/maps.googleapis.com/maps/api/staticmap?center='+LatLong+'&zoom=19&format=png32&size=640x640&maptype=satellite&key=AIzaSyBd7DMOTTilre43iTilXMGru34D15yHxO4', 'sputnik.png')
-        pal=download('https:/maps.googleapis.com/maps/api/staticmap?center='+LatLong+'&style=feature:all|element:labels|visibility:off&zoom=19&format=png32&size=640x640&key=AIzaSyBd7DMOTTilre43iTilXMGru34D15yHxO4', 'pam.png')
+        sputnik=getColors('https://maps.googleapis.com/maps/api/staticmap?center='+LatLong+'&zoom=19&format=png32&size=450x450&maptype=satellite&key=AIzaSyBd7DMOTTilre43iTilXMGru34D15yHxO4', 'may.png')
+        sputnikya=getColors('https://static-maps.yandex.ru/1.x/?ll='+str(Lat)+','+str(Long)+'&size=450,450&z=19&l=sat', 'AySat')
+        pal=getColors('https://maps.googleapis.com/maps/api/staticmap?center='+LatLong+'&style=feature:all|element:labels|visibility:off&zoom=19&format=png32&size=450x450&key=AIzaSyBd7DMOTTilre43iTilXMGru34D15yHxO4', 'pam.png')
         #downloading from sputnik
-        pixS=sputnik.load()#массив пикселей со спутника
-        pixM=pal.load()
-        draw = ImageDraw.Draw(sputnik)
-        for i in range(182):#массив пикселей
-            for j in range(332):
-                r = pixS[i, j][0]
-                g = pixS[i, j][1]
-                b = pixS[i, j][2]
-                if(r+b-g<75 and g>b and g>r):#условие для определения цвета пикселей с полученной со спутника растровой картинки
-                        draw.point((i, j), (30, 200,30))#цвет закраски
+        img = Image.open('Amap.png')
+        draw = ImageDraw.Draw(img)
+        for i in range(450):#массив пикселей
+            for j in range(450):
+                if (condition(sputnik[i, j]) or condition(sputnikya[i, j])) and  pal[i, j][0]==223 and pal[i, j][1]==223 and pal[i, j][2]==223:#условие для определения цвета пикселей с полученной со спутника растровой картинки
+                        draw.point((i, j), (0, 255,0))#цвет закраски
                 else:
-                        qdraw.point((i, j), (pixM[i,j][0], pixM[i,j][1],pixM[i,j][2]))#закраска ч/б в случае несоответствия с условием цикла
- 
+                    draw.point((i, j), sputnik[i, j])
+                        #draw.point((i, j), (pixM[i, j][0], pixM[i, j][1],pixM[i, j][2]))
         LatLong=str(x)+","+str(y)
-        img3 = sputnik.crop((0,0,182,332))
+        img3 = img.crop((0,0,182,332))
         img3.save("map("+LatLong+").png", "PNG")
         img3.save("map().png", "PNG")
         del draw#удаляем объект
